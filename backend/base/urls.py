c2422a3c665f4d4bce44bac31750dd567e67c373
ambadidi <amine.badidi@hotmail.com>
2025-05-22 06:58:06 +0100

    video 35

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+# )
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
+    path('users/profile', views.getUserProfile, name="users-profile"),
     path('patients/', views.getPatients, name="patients"),
     path('patients/<str:pk>/', views.getPatient, name="patient"),
     path('doctor/', views.getDoctor, name="doctor")
--- backend/base/views.py
+++ backend/base/views.py
@@ -7,6 +7,22 @@ from .models import *
 from .serializers import *
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -19,6 +35,12 @@ def getRoutes(request):
         '/api/patients/<id>/'
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getPatients(request):
